dbdafb16b2270b1cb295ca66fae6ed8142966474
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-08-05 00:17:50 +0300

    register Feed in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from main_app.models import SiteData
+from main_app.models import SiteData, Feed
 
 
 @admin.register(SiteData)
 class SiteDataAdmin(admin.ModelAdmin):
     list_display = ['header_title', 'created', 'updated']
 
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ["description", ]
+    list_filter = ["display", "created", "updated"]
